d9cdaaa083b7afbce8d0bfa03255985ffecff35b
Дастан <d_baimagambetov@kbtu.kz>
2023-03-29 17:00:05 +0600

    q

---
+++ app/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render, redirect
+
+def index(request):
+    if request.user.is_authenticated:
+        return redirect('/account')
+    else:
+        return render(request, 'index.html')
+
+
+def account(request):
+    user = request.user
+    return render(request, 'account.html', {'user': user})
---
+++ djangoProject6/urls.py
@@ -0,0 +1,18 @@
+
+from django.contrib import admin
+from django.urls import path
+from django.conf.urls.static import static
+
+from app import views, views_auth
+from djangoProject6 import settings
+
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('admin/', admin.site.urls),
+    path('register/', views_auth.registration, name='register'),
+    path('login/', views_auth.login_view, name='login'),
+    path('logout/', views_auth.logout_view, name='logout'),
+    path('account/', views.account, name='account'),
+]
+urlpatterns+=static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
